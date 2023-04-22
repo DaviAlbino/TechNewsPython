05039bd9c9570aa39026551e47ac7ac1a15e9774
Davi Albino <negroalbino_@hotmail.com>
2023-04-22 10:16:21 -0300

    feat: fetch url

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,17 @@
+import requests
+import time
+
+
 # Requisito 1 - iniciando o projeto
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=3)
+        time.sleep(1)
+        response.raise_for_status()
+    except (requests.HTTPError, requests.ReadTimeout):
+        return None
+    else:
+        return response.text
 
 
 # Requisito 2
