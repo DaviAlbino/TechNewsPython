1b94a5b60240c02b6a6c7761ad3dc0079f84e8b9
Davi Albino <negroalbino_@hotmail.com>
2023-04-22 22:51:54 -0300

    feat: seach by title and category

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,13 @@
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    found_news = []
+    news_list = search_news({"title": {"$regex": title, "$options": "i"}})
+    for piece in news_list:
+        found_news.append((piece["title"], piece["url"]))
+    return found_news
 
 
 # Requisito 8
@@ -10,4 +17,8 @@ def search_by_date(date):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    found_news = []
+    news_li = search_news({"category": {"$regex": category, "$options": "i"}})
+    for piece in news_li:
+        found_news.append((piece["title"], piece["url"]))
+    return found_news
